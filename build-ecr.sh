844c38446e57c37dc5e488ecea4ea2f6b45dbd69
Jim Clark <jim@atomist.com>
2021-03-12 15:37:28 -0800

    bump

--- build-ecr.sh
+++ build-ecr.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-echo build base:latest
-
 docker build \
   --build-arg REVISION=$(git rev-parse HEAD) \
   --build-arg SOURCE=Dockerfile.base \
   -t "$ECR_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/base:latest" \
   -f base/Dockerfile .
 
-echo build service:latest
 docker build \
   --build-arg REVISION=$(git rev-parse HEAD) \
   --build-arg SOURCE=Dockerfile.service \
@@ -17,9 +14,7 @@ docker build \
 
 aws ecr get-login-password --region us-east-1 --profile sts | docker login --username AWS --password-stdin 111664719423.dkr.ecr.us-east-1.amazonaws.com
 
-echo push base:latest
 docker push "$ECR_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/base:latest"
 
-echo push service:latest
 docker push "$ECR_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/service:latest"
 
