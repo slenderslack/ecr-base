2c068e94b506feb4e5facccc140368bca64d3724
Jim Clark <jim@atomist.com>
2021-05-11 15:44:26 -0700

    bump

--- build-ecr.sh
+++ build-ecr.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+aws ecr get-login-password --region us-east-1 --profile sts | docker login --username AWS --password-stdin "$ECR_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com"
+
 docker build \
   --build-arg REVISION=$(git rev-parse HEAD) \
   --build-arg SOURCE=base/Dockerfile \
   -t "$ECR_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/base:latest" \
   -f base/Dockerfile .
 
+docker push "$ECR_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/base:latest"
+
+echo "wait for 20 seconds"
+
+sleep 20
+
 docker build \
   --build-arg REVISION=$(git rev-parse HEAD) \
   --build-arg SOURCE=service/Dockerfile \
   -t "$ECR_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/service:latest" \
   -f service/Dockerfile .
-
-aws ecr get-login-password --region us-east-1 --profile sts | docker login --username AWS --password-stdin "$ECR_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com"
-
-docker push "$ECR_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/base:latest"
 
 docker push "$ECR_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/service:latest"
 
